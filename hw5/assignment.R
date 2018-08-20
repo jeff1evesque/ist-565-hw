2e042127ea06caf81954780a8ddc7dbae0ee257a
jeffrey levesque <jeff1evesque@yahoo.com>
2018-08-20 18:47:23 -0400

    #69: assignment.R, add initial file

---
+++ hw5/assignment.R
@@ -0,0 +1,22 @@
+##
+## assignment.R
+##
+
+## set project cwd: only execute in RStudio
+if (nzchar(Sys.getenv('RSTUDIO_USER_IDENTITY'))) {
+  cwd = dirname(dirname(rstudioapi::getSourceEditorContext()$path))
+  setwd(cwd)
+}
+
+## create ignored directories
+dir.create(file.path(cwd, 'hw5/visualization'), showWarnings = FALSE)
+
+## load custom package
+devtools::install_local(paste(cwd, sep='', '/packages/loadPackage'))
+library('loadPackage', 'factoextra')
+
+## load contrib packages
+load_package(c(''))
+
+## import dataset
+df = read.csv('data/fedPapers/fedPapers85.csv')
