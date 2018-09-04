12d2bb715b1c7cee109ef3ae95352bcd8f9e524f
jeffrey levesque <jeff1evesque@yahoo.com>
2018-09-04 19:27:04 -0400

    #32: assignment.R, add initial file

---
+++ hw6/assignment.R
@@ -0,0 +1,23 @@
+##
+## assignment.R
+##
+
+## set project cwd: only execute in RStudio
+if (nzchar(Sys.getenv('RSTUDIO_USER_IDENTITY'))) {
+  cwd = dirname(dirname(rstudioapi::getSourceEditorContext()$path))
+  setwd(cwd)
+}
+
+## create ignored directories
+dir.create(file.path(cwd, 'hw6/visualization'), showWarnings = FALSE)
+
+## load custom package
+devtools::install_local(paste(cwd, sep='', '/packages/loadPackage'))
+library('loadPackage')
+
+## load contrib packages
+load_package(c())
+
+## import dataset
+df.train = read.csv('data/digit--train.csv')
+df.test = read.csv('data/digit--test.csv')
