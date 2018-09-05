7cb3c2846a4e5680cbd31525c4e8d43bc022f0b9
jeffrey levesque <jeff1evesque@yahoo.com>
2018-09-04 20:13:54 -0400

    #95: assignment.R, fit + plot decision tree

--- hw6/assignment.R
+++ hw6/assignment.R
@@ -16,8 +16,22 @@ devtools::install_local(paste(cwd, sep='', '/packages/loadPackage'))
 library('loadPackage')
 
 ## load contrib packages
-load_package(c())
+load_package(c('rpart', 'rpart.plot'))
 
 ## import dataset
 df.train = read.csv('data/digit--train.csv')
 df.test = read.csv('data/digit--test.csv')
+
+##
+## decision tree
+##
+fit.tree = rpart(
+  label ~ .,
+  data = df.train,
+  method = 'class'
+)
+
+## visualize default tree
+png('hw6/visualization/default_tree.png')
+rpart.plot(fit.tree)
+dev.off()
