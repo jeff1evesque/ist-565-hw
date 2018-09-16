2d514a3b8decd2c43d66ed378879dddef4f53934
jeffrey levesque <jeff1evesque@yahoo.com>
2018-09-15 23:32:23 -0400

    #122: assignment.R, add initial file

---
+++ hw8/assignment.R
@@ -0,0 +1,22 @@
+##
+## assignment.R
+##
+
+## set project cwd: only execute in RStudio
+if (nzchar(Sys.getenv('RSTUDIO_USER_IDENTITY'))) {
+  cwd = dirname(dirname(rstudioapi::getSourceEditorContext()$path))
+  setwd(cwd)
+}
+
+## create ignored directories
+dir.create(file.path(cwd, 'hw8/visualization'), showWarnings = FALSE)
+
+## load custom package
+devtools::install_local(paste(cwd, sep='', '/packages/loadPackage'))
+library('loadPackage')
+
+## load contrib packages
+load_package(c(''))
+
+## import dataset
+df.train.full = read.csv('data/deception_data_converted_final.csv')
