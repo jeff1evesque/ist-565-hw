f778cec45cbcfdffc645fda69d513b0231564722
jeffrey levesque <jeff1evesque@yahoo.com>
2018-09-16 08:13:54 -0400

    #122: assignment.R, use 'stringi' to split columns

--- hw8/assignment.R
+++ hw8/assignment.R
@@ -16,7 +16,18 @@ devtools::install_local(paste(cwd, sep='', '/packages/loadPackage'))
 library('loadPackage')
 
 ## load contrib packages
-load_package(c(''))
+load_package(c('stringi'))
 
 ## import dataset
-df.train.full = read.csv('data/deception_data_converted_final.csv')
+df.colnames = read.table('data/deception_data_converted_final.csv', nrow=1, stringsAsFactors=FALSE, sep=',')
+df.full = read.table('data/deception_data_converted_final.csv', skip=1,header=FALSE, sep='\n')
+
+##
+## separate columns: match first two instances of commas, and split
+##     into three different columns.
+##
+out = stri_split_fixed(str = df.full[, c(1)], pattern = ',', n = 3)
+df.split = as.data.frame(do.call(rbind, out))
+
+## assign column name
+colnames(df.split) = df.colnames
