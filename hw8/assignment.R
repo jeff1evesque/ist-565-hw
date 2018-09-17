7a28d925bc9e5f4108a013ea12d6b3e68f3f211d
jeffrey levesque <jeff1evesque@yahoo.com>
2018-09-17 17:37:14 -0400

    #128: assignment.R, create dtm with tfidf

--- hw8/assignment.R
+++ hw8/assignment.R
@@ -16,7 +16,7 @@ devtools::install_local(paste(cwd, sep='', '/packages/loadPackage'))
 library('loadPackage')
 
 ## load contrib packages
-load_package(c('stringi'))
+load_package(c('stringi', 'naivebayes', 'text2vec'))
 
 ## import dataset
 filepath = 'data/deception_data_converted_final.csv'
@@ -40,6 +40,16 @@ df.split = as.data.frame(do.call(rbind, out))
 ## assign column name
 colnames(df.split) = df.colnames
 
+## tokenize
+tokens = word_tokenizer(tolower(df.split$review[1:nrow(df.split)]))
+
+## document term matrix
+dtm = create_dtm(itoken(tokens), hash_vectorizer())
+
+## term frequency-inverse document frequency
+model_tfidf = TfIdf$new()
+dtm_tfidf = model_tfidf$fit_transform(dtm)
+
 ##
 ## create train + test
 ##
@@ -50,4 +60,3 @@ sample_size = floor(2/3 * nrow(df.split))
 train = sample(seq_len(nrow(df.split)), size = sample_size)
 df.train = df.split[train, ]
 df.test = df.split[-train, ]
-
